b84de642d0349bad5cd2e8c37d8d865c9c1ba78c
Nils Hagemann <mail@nilshagemann.de>
2014-01-26 18:10:35 +0100

    - new form element number

---
+++ src/AnyContent/CMCK/Modules/Backend/Edit/NumberFormElement/FormElementNumber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Anycontent\CMCK\Modules\Backend\Edit\NumberFormElement;
+
+class FormElementNumber extends \AnyContent\CMCK\Modules\Backend\Core\Edit\FormElementDefault
+{
+
+    public function render($layout)
+    {
+        $outputFormatter = $this->getNumberFormatter();
+
+        $leftAddOn  = '#';
+        $rightAddOn = null;
+
+        if ($this->definition->getUnit())
+        {
+            $leftAddOn  = null;
+            $rightAddOn = $this->definition->getUnit();
+        }
+
+        $this->vars['leftAddOn']  = $leftAddOn;
+        $this->vars['rightAddOn'] = $rightAddOn;
+
+        $this->vars['value'] = $outputFormatter->format($this->vars['value']);
+
+        return $this->twig->render('formelement-number.twig', $this->vars);
+    }
+
+
+    public function parseFormInput($input)
+    {
+        $value = $this->getNumberFormatter()->parse($input);
+        $value = number_format($value, $this->definition->getDigits());
+
+        return $value;
+    }
+
+
+    protected function getNumberFormatter()
+    {
+        $numberFormatter = new \NumberFormatter(\Locale::getDefault(), \NumberFormatter::DECIMAL);
+        $numberFormatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $this->definition->getDigits());
+        $numberFormatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $this->definition->getDigits());
+
+        return $numberFormatter;
+    }
+}
--- web/index.php
+++ web/index.php
@@ -28,10 +28,13 @@ $app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\TextFormElements');
 $app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\PartitionFormElements');
 $app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\RichtextTinyMCEFormElements');
 $app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\SourceCodeFormElements');
+$app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\SelectionFormElements');
+$app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\NumberFormElement');
+$app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\DateTimeFormElements');//array('Format.Long'=>'d.m.Y','Format.Short'=>'d.m','Format.DateTime'=>'d.m.Y H:i','Format.Full'=>'d.m.Y H:i:s')
 $app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\SequenceFormElement');
 $app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\InsertFormElement');
-$app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\SelectionFormElements');
-$app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\DateTimeFormElements');//array('Format.Long'=>'d.m.Y','Format.Short'=>'d.m','Format.DateTime'=>'d.m.Y H:i','Format.Full'=>'d.m.Y H:i:s')
+
+
 
 $app->registerModule('AnyContent\CMCK\Modules\Backend\Edit\Export',array('FormatCode.DateTime'=>'d.m.YYYY hh:mm'));
 
