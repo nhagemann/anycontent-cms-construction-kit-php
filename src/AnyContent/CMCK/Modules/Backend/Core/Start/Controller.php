b4805cd78221f104cb0c400a6f90baaa98c7eb1d
Nils Hagemann <mail@nilshagemann.de>
2014-03-05 15:58:53 +0100

    - module start for / route

---
+++ src/AnyContent/CMCK/Modules/Backend/Core/Start/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AnyContent\CMCK\Modules\Backend\Core\Start;
+
+use AnyContent\CMCK\Modules\Backend\Core\Application\Application;
+
+use CMDL\ContentTypeDefinition;
+use CMDL\ClippingDefinition;
+
+use AnyContent\Client\Repository;
+use AnyContent\Client\Record;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class Controller
+{
+
+    public static function index(Application $app)
+    {
+        $vars = array();
+
+        $vars['menu_mainmenu'] = $app['menus']->renderMainMenu();
+
+        return $app->renderPage('start.twig', $vars);
+    }
+}
---
+++ src/AnyContent/CMCK/Modules/Backend/Core/Start/Module.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AnyContent\CMCK\Modules\Backend\Core\Start;
+
+use AnyContent\CMCK\Modules\Backend\Core\Application\Application;
+
+use AnyContent\CMCK\Modules\Backend\Core\Edit\FormManager;
+
+class Module extends \AnyContent\CMCK\Modules\Backend\Core\Core\Module
+{
+
+    public function init(Application $app, $options = array())
+    {
+        parent::init($app, $options);
+
+        $app->addTemplatesFolders(__DIR__ . '/views/');
+
+        $app->get('/', 'AnyContent\CMCK\Modules\Backend\Core\Start\Controller::index');
+    }
+
+
+    public function run(Application $app)
+    {
+    }
+}
